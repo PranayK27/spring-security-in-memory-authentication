ba3b5d237feedc62bad5a0c675e031085ffd21a0
Pranay <kekre.pranay@gmail.com>
2022-04-23 10:30:17 +0100

    in-memory-spring-security

---
+++ src/main/java/com/pranay/springsecurityinmemoryauthentication/SpringSecurityInMemoryAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.pranay.springsecurityinmemoryauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInMemoryAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInMemoryAuthenticationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/controller/SecurityController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/home")
+    public ModelAndView greeting() {
+       return new ModelAndView("home");
+    }
+}
+
